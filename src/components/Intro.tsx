102ae9bcb9a843fa7aaf60c8323f175e2531a10c
Daniel <brucki2002@gmail.com>
2023-03-14 21:39:47 +0100

    scss components

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -7,6 +7,12 @@ export default function Intro() {
 
     return (
         <div className="intro">
+            <div>
+                <h3 className="intro__author subtitle">made by Daniel Bruckner</h3>
+                <h1 className="intro__title title">Pick your gifs</h1>
+                <p className="intro__description paragraph">GIFs are popular on the internet for conveying emotions, reactions, or conveying a message in a humorous or creative way. </p>
+                <a className="intro__btn btn" aria-label="button" href="#search">Discover gifs</a>
+            </div>
             <img src={heroImage} />
         </div>
     )
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function SearchBar() {
 
     return (
-        <div className="searchBar">
+        <div className="searchBar" id="search">
             <h1>SearchBar</h1>
         </div>
     )
