7c7517a55bb6811c0e09fab040d71106cc7df82e
Daniel <brucki2002@gmail.com>
2023-03-15 20:28:48 +0100

    basic setup, html, scss, responive
    no fetch component yet

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -6,7 +6,7 @@ import heroImage from "../assets/upscaledVersionChallenge.jpg"
 export default function Intro() {
 
     return (
-        <div className="intro gutter-medium">
+        <section className="intro gutter-medium">
             <div className="intro__container-text">
                 <h3 className="intro__author subtitle">made by Daniel Bruckner</h3>
                 <h1 className="intro__title title">Pick your <br/> gifs</h1>
@@ -14,6 +14,6 @@ export default function Intro() {
                 <a className="intro__btn btn" aria-label="button" href="#search">Discover gifs</a>
             </div>
             <img className="intro__hero-img" src={heroImage} />
-        </div>
+        </section>
     )
 }
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -3,8 +3,17 @@ import React from "react"
 export default function SearchBar() {
 
     return (
-        <div className="searchBar" id="search">
-            <h1>SearchBar</h1>
-        </div>
+        <section className="searchBar gutter-medium" id="search">
+            <form className="searchBar__form">
+                <input 
+                className="searchBar__input" 
+                placeholder="Find gifs" 
+                />
+                <button className="searchBar__btn btn btn--light">Search</button>
+            </form>
+            <div className="searchBar__container-results">
+
+            </div>
+        </section>
     )
 }
